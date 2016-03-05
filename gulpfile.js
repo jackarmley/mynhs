553f3e09d5d9456dacf1780e58c7c07c1615c23c
jackarmley <jackarmley@gmail.com>
2016-03-05 17:56:00 +0000

    Local server gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    path = require('path');
+    path = require('path')
+    connect = require('gulp-connect');
 
 var assets = {
     styles : './node_modules/bootstrap/less/'
@@ -14,11 +15,19 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('server', function(){
+  connect.server({
+    port: 3000,
+    root: 'public'
+  });
+});
+
 gulp.task('watch',function(){
   gulp.watch(assets.styles + '**/*.less',['less']);
 });
 
 gulp.task('default', [
+    'server',
     'less',
     'watch'
 ]);
