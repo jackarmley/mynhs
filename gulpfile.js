cf42a77a1fe8dfcc995d42151e0a35834f7f63e1
jackarmley <jackarmley@gmail.com>
2016-03-05 17:50:03 +0000

    Added a watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     path = require('path');
 
+var assets = {
+    styles : './node_modules/bootstrap/less/'
+}
+
 gulp.task('less', function () {
   return gulp.src('./node_modules/bootstrap/less/bootstrap.less')
     .pipe(less({
@@ -10,6 +14,13 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('watch',function(){
+  gulp.watch(assets.styles + '**/*.less',['less']);
+});
+
 gulp.task('default', [
-    'less'
+    'less',
+    'watch'
 ]);
+
+
